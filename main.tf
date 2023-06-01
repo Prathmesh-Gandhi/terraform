fbf840d8295f4a72440842e788ee1715485add30
Prathmesh-Gandhi <113703326+Prathmesh-Gandhi@users.noreply.github.com>
2023-06-01 16:53:24 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-east1-b"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "prod-subnet"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
